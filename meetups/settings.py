17f0eb9ba947894e7c61c61d8d989bb4912d3dbd
Bill Cromie <bill@cromie.org>
2021-02-22 11:56:40 -0500

    using dj_database_url for database config

--- meetups/settings.py
+++ meetups/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 import sys
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -82,10 +83,7 @@ WSGI_APPLICATION = "meetups.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "slack-meetups.db")
-    }
+    "default": dj_database_url.config(conn_max_age=600)
 }
 
 
